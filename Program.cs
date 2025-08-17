282816e95d2bb00cf927e250d9cd43cbadc7aaa0
Hugop2san <hugopereira2santos60@gmail.com>
2025-08-17 18:43:55 -0300

    Demonstração inicial de linq com where e select

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace treinolinq
+{
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+            int[] numbers = new int[]
+            {
+                1, 2, 3, 4, 5, 6, 8, 9, 10
+            };
+
+            var result = numbers.Where(x => x % 2 == 0).Select(x => x*10);
+            // seleciona com lambda apenas os pares e depois multiplico por 10.
+
+            foreach(var i in result)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
